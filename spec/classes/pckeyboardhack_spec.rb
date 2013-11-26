448461538a5deed1376d73a7d345b7e2018240b6
Byron Wong <byronmwong@gmail.com>
2013-11-25 21:43:58 -0500

    bump version to 10.2.0

--- spec/classes/pckeyboardhack_spec.rb
+++ spec/classes/pckeyboardhack_spec.rb
@@ -4,9 +4,9 @@ describe 'pckeyboardhack' do
   it do
     should include_class('pckeyboardhack::config')
 
-    should contain_package('PCKeyboardHack_10.0.0').with({
+    should contain_package('PCKeyboardHack_10.2.0').with({
       :ensure   => 'installed',
-      :source   => 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.0.0.dmg',
+      :source   => 'https://pqrs.org/macosx/keyremap4macbook/files/PCKeyboardHack-10.2.0.dmg',
       :provider => 'pkgdmg'
     })
   end
